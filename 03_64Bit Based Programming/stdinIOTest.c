63f92791f182c4f83caa4b5051aa3bda4a08ecc5
Lee Garam <agerio100@naver.com>
2023-12-25 13:31:56 +0900

    Update stdinIOTest.c

--- 03_64Bit Based Programming/stdinIOTest.c
+++ 03_64Bit Based Programming/stdinIOTest.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <tchar.h>
-#include <windows.h>
+#include <Windows.h>
 
 #define MAX_CMD_LENGTH 256
 
 int CmdProcessing(void) {
-    _fputts(_T("Command Here UwU >> "), stdout);
+    _fputts(_T("Input here...> "), stdout);
 
     // Use an array to store the input
     TCHAR cmdString[MAX_CMD_LENGTH];
+    TCHAR* separators = _T(" \t\n"); 
 
     if (_fgetts(cmdString, MAX_CMD_LENGTH, stdin) != NULL) {
         // Process the input as needed
         _tprintf(_T("You entered: %s"), cmdString);
 
         // Tokenize the input if necessary
-        TCHAR* separators = _T(" \t\n");  // Add more separators if needed
+        
         TCHAR* token = _tcstok(cmdString, separators);
         UINT tokenNum = 0;
 
         while (token != NULL) {
-            _tprintf(_T("Token %u: %s\n"), tokenNum, token);
+            _tprintf(_T("Token[%u] => %s\n"), tokenNum, token);
             token = _tcstok(NULL, separators);
             ++tokenNum;
         }
-    }
-    else {
+    } else {
         _tprintf(_T("Error reading input.\n"));
     }
 
