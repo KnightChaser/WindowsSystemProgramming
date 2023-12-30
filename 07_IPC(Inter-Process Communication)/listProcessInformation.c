3e730bfef7fbea00c9c09eb40a7c3701ed2fe655
Lee Garam <agerio100@naver.com>
2023-12-31 00:27:11 +0900

    Changed to function style

--- 07_IPC(Inter-Process Communication)/listProcessInformation.c
+++ 07_IPC(Inter-Process Communication)/listProcessInformation.c
@@ -6,13 +6,26 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
+BOOL listProcessInformation();
+
 int _tmain(int argc, TCHAR* argv[]) {
 
+	listProcessInformation();
+	return 0;
+
+}
+
+/// <summary>
+///  Take a snapshot of the current process information in the device when this called by something else
+///  and print every process information record in row
+/// </summary>
+/// <returns>FALSE if the task fails, TRUE if the task succeeds</returns>
+BOOL listProcessInformation() {
 	// Take a snapshot of the current every working process(TH32CS_SNAPPROCESS)
 	HANDLE hProcessSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	if (hProcessSnapshot == INVALID_HANDLE_VALUE) {
 		_tprintf(_T("CreateToolhelp32Snapshot() got an error! Error code: %d\n"), GetLastError());
-		return -1;
+		return FALSE;
 	}
 
 	// A struct object to obtain process information
@@ -22,7 +35,7 @@ int _tmain(int argc, TCHAR* argv[]) {
 	if (!Process32First(hProcessSnapshot, &processEntry32)) {
 		_tprintf(_T("Process32First() got an error! Error code: %d\n"), GetLastError());
 		CloseHandle(hProcessSnapshot);
-		return -1;
+		return FALSE;
 	}
 
 	do {
@@ -31,6 +44,5 @@ int _tmain(int argc, TCHAR* argv[]) {
 	} while (Process32Next(hProcessSnapshot, &processEntry32));
 
 	CloseHandle(hProcessSnapshot);
-	return 0;
-
+	return TRUE;
 }
