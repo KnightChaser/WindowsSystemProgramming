1e3b315ed7afdc7f6112c545a19c99f539ffbcf1
Lee Garam <agerio100@naver.com>
2024-01-05 18:56:09 +0900

    Update manualResetTimerPeriodic.c

--- 13_Thread Synchronization/manualResetTimerPeriodic.c
+++ 13_Thread Synchronization/manualResetTimerPeriodic.c
@@ -7,7 +7,7 @@
 int _tmain(int argc, TCHAR* argv[]) {
 	HANDLE hTimer = NULL;
 	LARGE_INTEGER  timerEventDueTime;
-	timerEventDueTime.QuadPart = -100000000;	// relative time(e.g. X time unit since now) is expressed as negative value
+	timerEventDueTime.QuadPart = -20000000;	// relative time(e.g. X time unit since now) is expressed as negative value
 
 	// Create a timer object
 	hTimer = CreateWaitableTimer(NULL, FALSE, _T("WaitableTimer"));
@@ -16,14 +16,21 @@ int _tmain(int argc, TCHAR* argv[]) {
 		return -1;
 	}
 
-	_fputts(_T("Set SetWaitableTimer() for 10 seconds.\n"), stdout);
-	SetWaitableTimer(hTimer, &timerEventDueTime, 5000, NULL, NULL, FALSE);	// 10 seconds later, the alarm will be sent every 5 seconds
+	_fputts(_T("Set SetWaitableTimer() for 5 seconds.\n"), stdout);
+	SetWaitableTimer(hTimer, &timerEventDueTime, 1000, NULL, NULL, FALSE);	// 2 seconds later, the alarm will be sent every 1 seconds
 
 	// Wait for timer alarm
+	DWORD timerTriggerQty = 0;
 	while (TRUE) {
 		WaitForSingleObject(hTimer, INFINITE);
-		_fputts(_T("The periodic timer was signaled!\n"), stdout);
+		timerTriggerQty += 1;
+		_tprintf(_T("The periodic timer was signaled for %d times!\n"), timerTriggerQty);
 		MessageBeep(MB_ICONEXCLAMATION);
+		if (timerTriggerQty >= 10) {
+			CancelWaitableTimer(hTimer);
+			CloseHandle(hTimer);
+			break;
+		}
 	}
 
 	return 0;
