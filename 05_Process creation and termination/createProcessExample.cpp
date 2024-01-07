152a5d85aefa1b099bbaf5ef377a86cc578b03cd
Lee Garam <agerio100@naver.com>
2024-01-07 17:04:30 +0900

    Update createProcessExample.cpp

--- 05_Process creation and termination/createProcessExample.cpp
+++ 05_Process creation and termination/createProcessExample.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
 #include <string>
+#include <tchar.h>
 #include <windows.h>
 
-int main() {
+int _tmain(int argc, TCHAR* argv[]) {
     STARTUPINFO startupInformation;
     ZeroMemory(&startupInformation, sizeof(startupInformation));
     startupInformation.cb = sizeof(startupInformation);
     PROCESS_INFORMATION processInformation;
     ZeroMemory(&processInformation, sizeof(processInformation));
-    
+
     std::wstring command = L"calc.exe";
     SetCurrentDirectory(L"C:\\WINDOWS\\system32");
 
-    if (CreateProcess(NULL,
-        const_cast<LPWSTR>(command.c_str()),
-        NULL,
-        NULL,
-        TRUE,
-        0,
-        NULL,
-        NULL,
-        &startupInformation,
-        &processInformation))
-    {
-        // Wait for the process to finish (optional)
-        WaitForSingleObject(processInformation.hProcess, INFINITE);
 
-        // Close process and thread handles
-        CloseHandle(processInformation.hProcess);
-        CloseHandle(processInformation.hThread);
-    }
-    else
-    {
+    bool isProcessSuccessfullyCreated
+        = CreateProcess(NULL,                                   // lpApplicationName
+                        const_cast<LPTSTR>(command.c_str()),    // lpCommandLine
+                        NULL,                                   // lpProcessAttributes
+                        NULL,                                   // lpThreadAttributes
+                        TRUE,                                   // bInheritHandles
+                        0,                                      // dwCreationFlags
+                        NULL,                                   // lpEnvironment
+                        NULL,                                   // lpCurrentDirectory
+                        &startupInformation,                    // lpStartupInfo
+                        &processInformation);                   // lpProcessInformation
+    if (!isProcessSuccessfullyCreated) {
         // Handle the error, print an error message
         std::wcerr << L"Error creating process (Error " << GetLastError() << L") for " << command << std::endl;
     }
+
+    CloseHandle(processInformation.hProcess);
+    CloseHandle(processInformation.hThread);
 
     return 0;
 }
