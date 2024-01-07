400db93b637dc9540e398194ac178042aa1f4752
Lee Garam <agerio100@naver.com>
2024-01-07 17:54:30 +0900

    Use std::memset instead of ZeroMemory()
    Just being C++

--- 05_Process creation and termination/createProcessExample.cpp
+++ 05_Process creation and termination/createProcessExample.cpp
@@ -5,26 +5,26 @@
 
 int _tmain(int argc, TCHAR* argv[]) {
     STARTUPINFO startupInformation;
-    ZeroMemory(&startupInformation, sizeof(startupInformation));
+    std::memset(&startupInformation, 0, sizeof(startupInformation));
     startupInformation.cb = sizeof(startupInformation);
     PROCESS_INFORMATION processInformation;
-    ZeroMemory(&processInformation, sizeof(processInformation));
+    std::memset(&processInformation, 0, sizeof(processInformation));
 
     std::wstring command = L"calc.exe";
     SetCurrentDirectory(L"C:\\WINDOWS\\system32");
 
 
     bool isProcessSuccessfullyCreated
-        = CreateProcess(NULL,                                   // lpApplicationName
-                        const_cast<LPTSTR>(command.c_str()),    // lpCommandLine
-                        NULL,                                   // lpProcessAttributes
-                        NULL,                                   // lpThreadAttributes
-                        TRUE,                                   // bInheritHandles
-                        0,                                      // dwCreationFlags
-                        NULL,                                   // lpEnvironment
-                        NULL,                                   // lpCurrentDirectory
-                        &startupInformation,                    // lpStartupInfo
-                        &processInformation);                   // lpProcessInformation
+            = CreateProcess(NULL,                       // lpApplicationName
+                            const_cast<LPTSTR>(command.c_str()),    // lpCommandLine
+                            NULL,                                   // lpProcessAttributes
+                            NULL,                                   // lpThreadAttributes
+                            TRUE,                                   // bInheritHandles
+                            0,                                      // dwCreationFlags
+                            NULL,                                   // lpEnvironment
+                            NULL,                                   // lpCurrentDirectory
+                            &startupInformation,                    // lpStartupInfo
+                            &processInformation);                   // lpProcessInformation
     if (!isProcessSuccessfullyCreated) {
         // Handle the error, print an error message
         std::wcerr << L"Error creating process (Error " << GetLastError() << L") for " << command << std::endl;
